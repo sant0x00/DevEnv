3151e166585008c7c53f8b67e96017a804c19ea9
sant0x00 <santos0santos0@outlook.com>
2023-01-30 22:34:39 -0300

    feat(windows+linux): added folders to environment Linux and Windows

--- windows/set_windows_env.ps1
+++ windows/set_windows_env.ps1
@@ -1,20 +1,25 @@
-# Install the Git
+Write-Output "Trying to install Git..."
+
 Invoke-WebRequest -Uri https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1-64-bit.exe -OutFile git.exe
 Start-Process .\git.exe -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /NOICONS' -Wait
 
-# Install the JetBrains Toolbox
+Write-Output "Trying to install JetBrains Toolbox..."
+
 Invoke-WebRequest -Uri https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.18.6976.exe -OutFile toolbox.exe
 Start-Process .\toolbox.exe -Wait
 
-# Install the WSL2
+Write-Output "Trying to install WSL2..."
+
 dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
 wsl --set-default-version 2
 
-# Install the Docker Desktop
+Write-Output "Trying to install distribution Ubuntu..."
+
+wsl --import Ubuntu C:\Ubuntu\rootfs https://aka.ms/wsl-ubuntu-2004
+
+Write-Output "Trying to install Docker Desktop..."
+
 Invoke-WebRequest -Uri https://download.docker.com/win/stable/Docker%20Desktop%20Installer.exe -OutFile docker.exe
 Start-Process .\docker.exe -Wait
-
-# Install the distribution Ubuntu to the WSL2
-wsl --import Ubuntu C:\Ubuntu\rootfs https://aka.ms/wsl-ubuntu-2004
 
 Write-Output "All tools were installed!"
